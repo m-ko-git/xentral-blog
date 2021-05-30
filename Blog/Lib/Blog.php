88ca17c779eac620b2fa33c5ff2fc431fcf90872
Marco Grasshoff <marcograsshoff@Keymac-Lan-4.fritz.box>
2021-05-30 13:43:51 +0200

    Add start function as entry point into the blog

---
+++ Blog/Lib/Blog.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace Blog\Lib;
+
+
+/**
+ * Class Blog
+ * @package Blog\Lib
+ */
+class Blog
+{
+
+    /**
+     * From here we start all our Blog Logic
+     * (one entry point only)
+     */
+    public static function start()
+    {
+
+    }
+
+}
